878d860d14d6a48db7735ea8c7ed42fcb93dd6f5
Lars Flieger <a.purenetwork@gmail.com>
2021-08-10 23:16:12 +0200

    Add error for username is already taken

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, ObjectId } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -51,13 +51,20 @@ export class UserService {
    * @returns updated user (with changed fields)
    */
   async updateUser(id: ObjectId, updateUserDto: UpdateUserDto): Promise<User> {
-    const updatedUser: User = await this.userSchema.findByIdAndUpdate(id, {
-      ...updateUserDto
-    }, {
-      new: true
-    })
+    try {
+      const updatedUser: User = await this.userSchema.findByIdAndUpdate(id, {
+        ...updateUserDto
+      }, {
+        new: true
+      })
 
-    return updatedUser
+      return updatedUser
+    } catch(error) {
+      if(error.code === 11000)
+        throw new ConflictException('Username is already taken.')
+      else
+        throw new InternalServerErrorException()
+    }
   }
 
 
