96fcea8a7c0198ee9a8b603ccf66a538a4228192
Lars Flieger <a.purenetwork@gmail.com>
2021-08-15 15:17:49 +0200

    Add shared dtos

---
+++ src/shared/dto/mongoId.dto.ts
@@ -0,0 +1,9 @@
+import { IsMongoId } from "class-validator";
+import { ObjectId } from "mongoose";
+
+// Shared Dto to validate MongoId (We could create an own validation pipe for this)
+export class MongoIdDto {
+    
+    @IsMongoId()
+    id: ObjectId
+}
