b356df6948917e894ce92bfb01ae76582e853021
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2021-08-16 18:11:30 +0200

    search paginated, typespedific search enabled Password will not be sent with users

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,16 +1,21 @@
-import { Body, Controller, Get, Param, ParseArrayPipe, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseArrayPipe, ValidationPipe, Query } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { Query } from 'mongoose';
+import { PaginationDto } from 'src/shared/dto/pagination.dto';
 import { SearchService } from './search.service';
 
 @ApiTags('search')
 @Controller('search')
 export class SearchController {
-  constructor(private readonly searchService: SearchService) {}
+  constructor(private readonly searchService: SearchService) { }
 
   @Get(':searchstring')
-  @ApiOperation({summary: 'Searching for the send input in everything'})
-  search(@Param('searchstring') searchString: string){
-    return this.searchService.search(searchString)
+  @ApiOperation({ summary: 'Searching for the send input in everything' })
+  search(@Param('searchstring') searchString: string, @Query(new ValidationPipe({ transform: true })) paginationDto: PaginationDto) {
+    return this.searchService.search(searchString, +paginationDto.page, +paginationDto.limit)
+  }
+  @Get(':searchstring/:type')
+  @ApiOperation({ summary: 'Searching for the send input in everything' })
+  searchType(@Param('searchstring') searchString: string, @Param('type') type: string, @Query(new ValidationPipe({ transform: true })) paginationDto: PaginationDto) {
+    return this.searchService.search(searchString, +paginationDto.page, +paginationDto.limit, type)
   }
 }
--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -13,60 +13,103 @@ export class SearchService {
         @InjectModel('Task') private taskSchema: Model<TaskDocument>,
         @InjectModel('User') private userSchema: Model<UserDocument>) { }
 
-    async search(searchString: string) {
+    async search(searchString: string, page: number, limit: number, type = 'all') {
         if (searchString.length < 3) { throw new UnprocessableEntityException }
         const subStrings = searchString.toLowerCase().split(" ")
 
-        // Find in tasks content.message
+        console.log(type)
+        // all the variables setup
+        const skip = page * limit
+        limit += skip
+
+        let taskResult = []
+        let setResult = []
+        let userResult = []
         let regexQuery = []
-        // Only return active objects
-        regexQuery.push({ status: 'active' })
-        // Build a regex search for every substring
-        subStrings.forEach(string => {
-            regexQuery.push({ 'content.message': { $regex: RegExp(string), $options: 'i' } })
-        })
 
-        const taskResult = await this.taskSchema.find({ $and: regexQuery })
+        if (type == 'all' || type == 'task') {
+            // Find in tasks content.message
+            // Only return active objects
+            regexQuery.push({ status: 'active' })
+            // Build a regex search for every substring
+            subStrings.forEach(string => {
+                regexQuery.push({ 'content.message': { $regex: RegExp(string), $options: 'i' } })
+            })
 
-        // Find in sets name and description without the taskList!
-        regexQuery = [] // Setting below line as initial creates conflicts with adding new different object types into the array
-        // Only return active objects
-        regexQuery.push({ status: 'active' })
-        // Build a regex search for every substring
-        subStrings.forEach(string => {
-            regexQuery.push({ description: { $regex: RegExp(string), $options: 'i' } })
-        })
-        // Also checking name
-        let extraRegexQuery = []
-        extraRegexQuery.push({ status: 'active' })
-        subStrings.forEach(string => {
-            extraRegexQuery.push({ name: { $regex: RegExp(string), $options: 'i' } })
-        })
-        const setResult = await this.setSchema.aggregate([{ $unset: 'taskList' }, { $match: { $or: [{ $and: extraRegexQuery }, { $and: regexQuery }] } }])
-        // Consider also searching through the name with $or
+            taskResult = await this.taskSchema.aggregate([{ $match: { $and: regexQuery } }, { $limit: limit }, { $skip: skip }])
+            if (type == 'task') {
+                return {
+                    tasks: {
+                        itemsCount: taskResult.length,
+                        items: taskResult
+                    }
+                }
+            }
+        }
+
+        if (type == 'all' || type == 'set') {
+            // Find in sets name and description without the taskList!
+            regexQuery = [] // Setting below line as initial creates conflicts with adding new different object types into the array
+            // Only return active objects
+            regexQuery.push({ status: 'active' })
+            // Build a regex search for every substring
+            subStrings.forEach(string => {
+                regexQuery.push({ description: { $regex: RegExp(string), $options: 'i' } })
+            })
+            // Also checking name
+            let extraRegexQuery = []
+            extraRegexQuery.push({ status: 'active' })
+            subStrings.forEach(string => {
+                extraRegexQuery.push({ name: { $regex: RegExp(string), $options: 'i' } })
+            })
+            setResult = await this.setSchema.aggregate([{ $unset: 'taskList' }, { $match: { $or: [{ $and: extraRegexQuery }, { $and: regexQuery }] } }, { $limit: limit }, { $skip: skip }])
+            // Consider also searching through the name with $or
+
+            if (type == 'set') {
+                return {
+                    sets: {
+                        itemsCount: setResult.length,
+                        items: setResult
+                    }
+                }
+            }
+        }
+
+        if (type == 'all' || type == 'user') {
+            regexQuery = [] // Setting below line as initial creates conflicts with adding new different object types into the array
+            // Only return active objects
+            regexQuery.push({ status: 'active' })
+            // Build a regex search for every substring
+            subStrings.forEach(string => {
+                regexQuery.push({ username: { $regex: RegExp(string), $options: 'i' } })
+            })
+            userResult = await this.userSchema.aggregate([{ $match: { $and: regexQuery } }, { $limit: limit }, { $skip: skip },{$unset: 'password'}])
+
+            if (type == 'user') {
+                return {
+                    users: {
+                        itemsCount: userResult.length,
+                        items: userResult
+                    }
+                }
+            }
+        }
 
 
-        regexQuery = [] // Setting below line as initial creates conflicts with adding new different object types into the array
-        // Only return active objects
-        regexQuery.push({ status: 'active' })
-        // Build a regex search for every substring
-        subStrings.forEach(string => {
-            regexQuery.push({ username: { $regex: RegExp(string), $options: 'i' } })
-        })
-        const userResult = await this.userSchema.aggregate([{ $match: { $and: regexQuery } }])
-
-        return {
-            task: {
-                itemsCount: taskResult.length,
-                items: taskResult
-            },
-            set: {
-                itemsCount: setResult.length,
-                items: setResult
-            },
-            user: {
-                itemsCount: userResult.length,
-                items: userResult
+        if (type == 'all') {
+            return {
+                tasks: {
+                    itemsCount: taskResult.length,
+                    items: taskResult
+                },
+                sets: {
+                    itemsCount: setResult.length,
+                    items: setResult
+                },
+                users: {
+                    itemsCount: userResult.length,
+                    items: userResult
+                }
             }
         }
     }
