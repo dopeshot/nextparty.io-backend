6b8500ec779660ab9848e777b83602c14e993980
coffemakingtoaster <mh340@hdm-stuttgart.de>
2021-10-29 10:26:11 +0200

    rework mailer module

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,24 +1,15 @@
 import { Body, Controller, Get, Post, Render } from '@nestjs/common';
-import { render } from 'ejs';
 import { MailService } from './mail.service';
 
 @Controller('mail')
 export class MailController {
   constructor(private readonly mailService: MailService) {}
 
-  // In dev process we can leave this inside
+  // This is an endpoint sorely meant to be used for testing purposes. 
+  // Disable this once done testing or delete the controller enturely
   @Post()
-  async sendMail(
-    @Body('recipient') recipient: string) {
-    return await this.mailService.mailTest(recipient);
+  async sendMail(@Body('recipient') recipient: string) {
+    return await this.mailService.mailTest(recipient, "this is a dummy endpoint");
   }
 
-  // In dev process we can leave this inside
-  @Get()
-  @Render('index')
-  async test(
-    @Body('recipient') recipient: string) {
-    return
-    //return await this.mailService.mailTest(recipient);
-  }
 }
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,17 +1,32 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import { MailerService } from '@nestjs-modules/mailer';
-import { render, renderFile } from 'ejs'
+import { render} from 'ejs'
 import { readFile as _readFile } from 'fs';
 import { promisify } from 'util';
 import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
+import * as nodemailer from 'nodemailer';
 
 const readFile = promisify(_readFile);
 
+
+
 @Injectable()
 export class MailService {
-	constructor(private readonly mailerService: MailerService) {
-		mailerService
-	 }
+
+	mailTransport: nodemailer.Transporter<any>
+	
+	constructor(private readonly configService: ConfigService) {
+		this.mailTransport = nodemailer.createTransport({
+			host: process.env.MAIL_HOST,
+			port: parseInt(process.env.MAIL_HOST_PORT),
+			pool: true,
+			secure: false,
+			auth: {
+				user: process.env.MAIL_USER,
+				pass: process.env.MAIL_PASS
+			}
+		  })
+	}
 
 	/**
 	 * Method that communicates with mailserver to send mail
@@ -21,29 +36,28 @@ export class MailService {
 	 */
 	private async sendMail(recipient: string, subject: string, message: string): Promise<void> {
 		try {
-			await this.mailerService.sendMail({
+			await this.mailTransport.sendMail({
 				to: recipient,
 				from: 'watson.schulist@ethereal.email',
 				subject: subject,
 				html: message,
 			})
 		} catch(error) {
-			throw new InternalServerErrorException(`Error sending email with subject: ${subject}`)
+			throw new InternalServerErrorException(`Error sending email with subject: ${subject} with issue: ${error.message}`)
 		}
 	}
 
 	/**
 	 * Method called by the test endpoint
 	 * @param recipient - receiver for the email to be send
+	 * @param data - data to be displayed and rendered by ejs
 	 */
-	async mailTest(recipient: string) {
-		const template = await readFile(join(__dirname, 'templates', 'test.ejs'), 'utf-8')
-		const message = await renderFile('test.ejs', {
-			// Data to be sent to template engine.
-			code: 'cf1a3f828287',
-			username: 'john doe',
-		})
-		this.sendMail(recipient, "test3", message)
+	async mailTest(recipient: string, data: string): Promise<void> {
+		const tmpl = await readFile(__dirname + '/templates/test.ejs', 'utf-8')	
+		const message = render(tmpl, {
+			data: data
+		});
+		this.sendMail(recipient, "test", message)
 	}
 
 	/**
@@ -52,10 +66,10 @@ export class MailService {
 	 * @param mail - user mail
 	 * @param code - verification code
 	 */
-	async generateVerifyMail(name: string, mail: string, code: string){
+	async generateVerifyMail(name: string, mail: string, code: string): Promise<void>{
 		const template = await readFile(join(__dirname, 'templates', 'MailVerify.ejs'), 'utf-8')
 		const message = render(template, {
-			verifyLink: `${process.env.HOST}/api/user/verify/${code}`,
+			verifyLink: `${process.env.HOST}/user/verify/${code}`,
 			username: name,
 		});
 		this.sendMail(mail, "Please verify your email address", message)
@@ -67,10 +81,10 @@ export class MailService {
 	 * @param mail - user email
 	 * @param resetCode - the code required to reset/change the pw
 	 */
-	async sendPasswordReset(name: string, mail, resetCode){
+	async sendPasswordReset(name: string, mail: string, resetCode: string): Promise<void>{
 		const tmpl = await readFile(__dirname + '/templates/PasswordReset.ejs', 'utf-8')	
 		const message = render(tmpl, {
-			resetLink: `http://localhost:3000/api/user/reset-form/${resetCode}`,
+			resetLink: `${process.env.HOST}/user/reset-form/${resetCode}`,
 			username: name,
 		});
 		this.sendMail(mail, "Reset your pw", message)
