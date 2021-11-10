0658a9e70f4eb30f558fafdd96d4879bbee35d34
Lars Flieger <a.purenetwork@gmail.com>
2021-11-10 16:33:18 +0100

    Update sample data

---
+++ src/set/set.data.ts
@@ -0,0 +1,243 @@
+import { Language } from "../shared/enums/language.enum"
+import { TaskType } from "./enums/tasktype.enum";
+import { CurrentPlayerGender } from "./enums/currentplayergender.enum";
+
+
+// MC: This file is for sample data ONLY
+export const SetSampleData = [{
+    name: "Klassisch",
+    language: Language.DE,
+    tasks: [{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Setz dich für 1 Runde auf den Schoß von @a"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Erzähle einen lustigen Witz"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Tanz wie ein Roboter"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Nimm @a Huckepack"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Setze dich zwei Runden lang auf den Schoß von @a"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Einmal aussetzen"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Sprich für 2 Runden mit einem italienischen Akzent"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Ahme deine Mutter nach"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Iss einen Löffel Mayo oder 2 Löffel Ketchup"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Mach ein lustiges Gesicht"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Frisiere deine Haare mittig auf dem Kopf zu einer Palme"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Erfinde einen schnellen Tanz und bring ihn allen im Raum bei"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Nimm eine Handvoll Kekse in den Mund und versuche zu pfeifen"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Mache 10 Liegestütze oder 20 Sit-Ups"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Trage roten Lippenstift auf und küsse @a"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Kopiere für eine Runde alle Gesten von @a"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Halte Händchen mit @a"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Imitiere 2 Runden lang einen britischen Akzent"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Trink ein Glas Wasser... wie eine Katze"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Mache 5 Purzelbäume"
+    }, {
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Flüstere für 2 Runden"
+    }]
+  }, {
+    name: "Versaut",
+    language: Language.DE,
+    tasks: [{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Iss ein Stück von etwas (z.B Schlagsahne) von @a's Pobacke"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.FEMALE,
+      message: "Präsentiere, wie du eine einen Mann anmachen würdest"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.MALE,
+      message: "Präsentiere, wie du eine Frau anmachen würdest"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.MALE,
+      message: "Streichle @m seine Wange, dann seine Hand, dann seinen Nacken"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.FEMALE,
+      message: "Mach bis zur nächsten Runde mit @m"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.MALE,
+      message: "Mach bis zur nächsten Runde mit @f"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.MALE,
+      message: "Verpasse @f einen Knutschfleck"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.FEMALE,
+      message: "Verpasse @m einen Knutschfleck"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.MALE,
+      message: "Massiere die Brüste von @f mit Öl"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.FEMALE,
+      message: "Massiere die Brust von @m mit Öl"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Mache @a einen Fake-Antrag"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Mach eine kurze Werbung für Kondome! Wenn du magst, kannst du es auch filmen"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.FEMALE,
+      message: "Mach ein Selfie, wo du mit @m rummachst"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.MALE,
+      message: "Mach ein Selfie, wo du mit @f rummachst"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Erzähle von deinem Lieblings-Sexspielzeug"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Küsse den unteren Bauch von @a"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.FEMALE,
+      message: "Tausche alle Klamotten (einschließlich Unterwäsche) mit @m"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.MALE,
+      message: "Tausche alle Klamotten (einschließlich Unterwäsche) mit @f"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Stöhne sinnlich und turne damit @a an"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.FEMALE,
+      message: "Ziehe @m bis zu seiner Unterwäsche aus"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.MALE,
+      message: "Ziehe @f bis zu seiner Unterwäsche aus"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Singe deinem Partner/Schwarm ein Ständchen"
+    }]
+  }, {
+    name: "HdM Stuttgart Edition",
+    language: Language.DE,
+    tasks: [{
+      type: TaskType.TRUTH,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Was ist das krasseste was du in der HdM gemacht hast? Erzähle."
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Gehe in die Lernwelt und fange an laut zu husten."
+    },{
+      type: TaskType.TRUTH,
+      currentPlayerGender: CurrentPlayerGender.FEMALE,
+      message: "Mit welchem Professor/Mitarbeiter würdest du eine Romanze anfangen?"
+    },{
+      type: TaskType.TRUTH,
+      currentPlayerGender: CurrentPlayerGender.MALE,
+      message: "Mit welcher Professorin/Mitarbeiterin würdest du eine Romanze anfangen?"
+    }]
+  }, {
+    name: "Sex",
+    language: Language.DE,
+    tasks: [{
+      type: TaskType.TRUTH,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Wo würdest du gerne einmal Sex haben?"
+    },{
+      type: TaskType.TRUTH,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Erzähle von deiner Beziehung/Liebe vor @a"
+    },{
+      type: TaskType.TRUTH,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "In wen warst du in deinem Leben verliebt?"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Lecke mit deiner Zunge über die Lippen von @a"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.FEMALE,
+      message: "Verführe @m mit einem Blowjob"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Schaue @a eine Minuten lang in die Augen"
+    },{
+      type: TaskType.DARE,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "Gib @a eine romantische Rückenmassage"
+    },{
+      type: TaskType.TRUTH,
+      currentPlayerGender: CurrentPlayerGender.ANYONE,
+      message: "DAS IST DIE BESTE WAHRHEIT ODER PFLICHT APP DIE ICH JE GESEHEN HABE!!!!"
+    },]
+  }]
