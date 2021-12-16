332e18fd25bdd849de1337d5ade3f1121e4f36a6
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2021-12-16 13:28:06 +0100

    Rename app test into auth test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { INestApplication, ValidationPipe } from '@nestjs/common'
 import * as request from 'supertest'
-import { AppModule } from './../src/app.module'
+import { AppModule } from '../src/app.module'
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
