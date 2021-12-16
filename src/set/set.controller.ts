b9e1474bd5fcee93eb5ccef78aa568601d695356
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2021-12-16 13:28:41 +0100

    Fix imports

--- src/set/set.controller.ts
+++ src/set/set.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, HttpCode, NotImplementedException, Param, Patch, Post, Put, Query, Request, UseGuards, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, NotImplementedException, Param, Patch, Post, Put, Query, Request, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ObjectId } from 'mongoose';
 import { JwtUserDto } from 'src/auth/dto/jwt.dto';
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe, UseGuards, Request, Req, Render, Res , Response} from '@nestjs/common';
-import { UserService } from './user.service';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { ObjectId, Query } from 'mongoose';
-import { User } from './entities/user.entity';
-import { JwtAuthGuard } from '../auth/strategies/jwt/jwt-auth.guard';
-import { Roles } from '../auth/roles/roles.decorator';
-import { Role } from './enums/role.enum';
-import { RolesGuard } from '../auth/roles/roles.guard';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Render, Request, Res, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { ObjectId } from 'mongoose';
+import { Roles } from '../auth/roles/roles.decorator';
+import { RolesGuard } from '../auth/roles/roles.guard';
+import { JwtAuthGuard } from '../auth/strategies/jwt/jwt-auth.guard';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Role } from './enums/role.enum';
+import { UserService } from './user.service';
 
 @ApiTags('user')
 @Controller('user')
