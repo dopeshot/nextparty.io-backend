0380ddb5e52c91679980ddca571376b4fac4ab73
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2021-12-20 15:31:35 +0100

    Remove search module due to future feature

--- src/search/dto/search.dto.ts
+++
@@ -1,7 +0,0 @@
-import { Type } from "class-transformer";
-import { IsArray, IsMongoId, IsNotEmpty, IsString, Length, ValidateNested } from "class-validator";
-import { ObjectId } from "mongoose";
-
-export class SearchDto {
-    // For future usage if search via Json (Tags)
-}
