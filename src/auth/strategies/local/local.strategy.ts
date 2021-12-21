f1d6768f33bf99775477650220c09d335b17bf98
coffemakingtoaster <mh340@hdm-stuttgart.de>
2021-12-21 21:16:13 +0100

    resolve review issue

--- src/auth/strategies/local/local.strategy.ts
+++ src/auth/strategies/local/local.strategy.ts
@@ -1,16 +1,16 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common'
-import { PassportStrategy } from '@nestjs/passport'
-import { Strategy } from 'passport-local'
-import { AuthService } from '../../auth.service'
-import { User } from '../../../user/entities/user.entity'
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { AuthService } from '../../auth.service';
+import { User } from '../../../user/entities/user.entity';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
     constructor(private authService: AuthService) {
         super({
             usernameField: 'email',
-            role: 'role',
-        })
+            role: 'role'
+        });
     }
 
     /**
@@ -23,9 +23,9 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
         // Validate user and check if user still exists
         const user = await this.authService.validateUserWithEmailPassword(
             email,
-            password,
-        )
-        if (!user) throw new UnauthorizedException()
-        return user
+            password
+        );
+        if (!user) throw new UnauthorizedException();
+        return user;
     }
 }
