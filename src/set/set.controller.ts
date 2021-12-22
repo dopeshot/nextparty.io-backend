4a3bd0d6fe1e64af08c195bf88614b1e71953902
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2021-12-22 22:45:49 +0100

    Ref type for requests

--- src/set/set.controller.ts
+++ src/set/set.controller.ts
@@ -10,18 +10,18 @@ import {
     Put,
     Query,
     Request,
-    UseGuards,
-} from '@nestjs/common'
-import { ApiOperation, ApiTags } from '@nestjs/swagger'
-import { ObjectId } from 'mongoose'
-import { JwtUserDto } from 'src/auth/dto/jwt.dto'
-import { JwtAuthGuard } from '../auth/strategies/jwt/jwt-auth.guard'
-import { MongoIdDto } from '../shared/dto/mongoId.dto'
-import { CreateSetDto } from './dto/create-set.dto'
-import { CreateTaskDto } from './dto/create-task.dto'
-import { UpdateSetDto } from './dto/update-set.dto'
-import { UpdateTaskDto } from './dto/update-task.dto'
-import { SetService } from './set.service'
+    UseGuards
+} from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ObjectId } from 'mongoose';
+import { JwtUserDto } from 'src/auth/dto/jwt.dto';
+import { JwtAuthGuard } from '../auth/strategies/jwt/jwt-auth.guard';
+import { MongoIdDto } from '../shared/dto/mongoId.dto';
+import { CreateSetDto } from './dto/create-set.dto';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateSetDto } from './dto/update-set.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { SetService } from './set.service';
 
 @ApiTags('set')
 @Controller('set')
@@ -31,20 +31,23 @@ export class SetController {
     @Post()
     @UseGuards(JwtAuthGuard)
     @ApiOperation({ summary: 'Create new set' })
-    createSet(@Body() createSetDto: CreateSetDto, @Request() req) {
-        return this.setService.createSet(createSetDto, req.user)
+    createSet(
+        @Body() createSetDto: CreateSetDto,
+        @Request() { user }: ParameterDecorator & { user: JwtUserDto }
+    ) {
+        return this.setService.createSet(createSetDto, user);
     }
 
     @Get()
     @ApiOperation({ summary: 'Get all Sets' })
     getAllSets() {
-        return this.setService.getAllSets()
+        return this.setService.getAllSets();
     }
 
     @Get(':id')
     @ApiOperation({ summary: 'Get one Set by id' })
     getOneSet(@Param() { id }: MongoIdDto) {
-        return this.setService.getOneSet(id)
+        return this.setService.getOneSet(id);
     }
 
     @Patch(':id')
@@ -53,9 +56,9 @@ export class SetController {
     updateMeta(
         @Param() { id }: MongoIdDto,
         @Body() updateSetDto: UpdateSetDto,
-        @Request() { user }: ParameterDecorator & { user: JwtUserDto },
+        @Request() { user }: ParameterDecorator & { user: JwtUserDto }
     ) {
-        return this.setService.updateSetMetadata(id, updateSetDto, user)
+        return this.setService.updateSetMetadata(id, updateSetDto, user);
     }
 
     @Delete(':id')
@@ -66,9 +69,9 @@ export class SetController {
     deleteSet(
         @Param() { id }: MongoIdDto,
         @Query('type') type: string,
-        @Request() { user }: ParameterDecorator & { user: JwtUserDto },
+        @Request() { user }: ParameterDecorator & { user: JwtUserDto }
     ) {
-        return this.setService.deleteSet(id, type, user)
+        return this.setService.deleteSet(id, type, user);
     }
 
     // Tasks
@@ -79,9 +82,9 @@ export class SetController {
     createTask(
         @Param() { id }: MongoIdDto,
         @Body() createTaskDto: CreateTaskDto,
-        @Request() { user }: ParameterDecorator & { user: JwtUserDto },
+        @Request() { user }: ParameterDecorator & { user: JwtUserDto }
     ) {
-        return this.setService.createTask(id, createTaskDto, user)
+        return this.setService.createTask(id, createTaskDto, user);
     }
 
     @Put(':id/task/:taskid')
@@ -91,9 +94,9 @@ export class SetController {
         @Param('id') setId: ObjectId,
         @Param('taskid') taskId: ObjectId,
         @Body() updateTaskDto: UpdateTaskDto,
-        @Request() { user }: ParameterDecorator & { user: JwtUserDto },
+        @Request() { user }: ParameterDecorator & { user: JwtUserDto }
     ) {
-        return this.setService.updateTask(setId, taskId, updateTaskDto, user)
+        return this.setService.updateTask(setId, taskId, updateTaskDto, user);
     }
 
     @Delete(':id/task/:taskid')
@@ -104,15 +107,18 @@ export class SetController {
         @Param('id') setId: ObjectId,
         @Param('taskid') taskId: ObjectId,
         @Query('type') type: string,
-        @Request() { user }: ParameterDecorator & { user: JwtUserDto },
+        @Request() { user }: ParameterDecorator & { user: JwtUserDto }
     ) {
-        return this.setService.removeTask(setId, taskId, type, user)
+        return this.setService.removeTask(setId, taskId, type, user);
     }
     /* istanbul ignore next */ // This is development only
     @Post('migrate')
     @UseGuards(JwtAuthGuard)
     @ApiOperation({ summary: 'Create example data sets' })
-    public createExampelData(@Request() req, @Query('test') test: string) {
-        return this.setService.createExampleSets(req.user, test)
+    public createExampelData(
+        @Request() { user }: ParameterDecorator & { user: JwtUserDto },
+        @Query('test') test: string
+    ) {
+        return this.setService.createExampleSets(user, test);
     }
 }
