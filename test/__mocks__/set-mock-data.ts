fa0e62105f8849b1ad378c7725048ddd5a38e695
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2021-12-29 18:20:29 +0100

    Edit tests for new visibility field in set.entity

--- test/__mocks__/set-mock-data.ts
+++ test/__mocks__/set-mock-data.ts
@@ -1,30 +1,76 @@
 import { CurrentPlayerGender } from '../../src/set/enums/currentplayergender.enum';
-import { SetCategory } from '../../src/set/enums/setcategory';
+import { SetCategory } from '../../src/set/enums/setcategory.enum';
 import { TaskType } from '../../src/set/enums/tasktype.enum';
+import { Visibility } from '../../src/set/enums/visibility.enum';
 import { Language } from '../../src/shared/enums/language.enum';
 import { Status } from '../../src/shared/enums/status.enum';
 import { Role } from '../../src/user/enums/role.enum';
 
-export const getSetSetupData = () => ({
-    _id: 'aaaaaaaaaaaaaaaaaaaaaaaa',
-    status: Status.ACTIVE,
-    language: Language.EN,
-    category: SetCategory.CLASSIC,
-    played: 0,
-    name: 'User Set',
-    createdBy: 'aaaaaaaaaaaaaaaaaaaaaaac',
-    dareCount: 0,
-    truthCount: 1,
-    tasks: [
-        {
-            _id: 'aaaaaaaaaaaaaaaaaaaaaaba',
-            status: Status.ACTIVE,
-            type: TaskType.TRUTH,
-            currentPlayerGender: CurrentPlayerGender.ANYONE,
-            message: '1234567890'
-        }
-    ]
-});
+export const getSetSetupData = () => [
+    {
+        _id: 'aaaaaaaaaaaaaaaaaaaaaaaa',
+        status: Status.ACTIVE,
+        language: Language.EN,
+        category: SetCategory.CLASSIC,
+        visibility: Visibility.PUBLIC,
+        played: 0,
+        name: 'User Set1',
+        createdBy: 'aaaaaaaaaaaaaaaaaaaaaaac',
+        dareCount: 0,
+        truthCount: 1,
+        tasks: [
+            {
+                _id: 'aaaaaaaaaaaaaaaaaaaaaaba',
+                status: Status.ACTIVE,
+                type: TaskType.TRUTH,
+                currentPlayerGender: CurrentPlayerGender.ANYONE,
+                message: '1234567890'
+            }
+        ]
+    },
+    {
+        _id: 'aaaaaaaaaaaaaaaaaaaaaaab',
+        status: Status.ACTIVE,
+        language: Language.EN,
+        category: SetCategory.CLASSIC,
+        visibility: Visibility.PRIVATE,
+        played: 0,
+        name: 'User Set2',
+        createdBy: 'aaaaaaaaaaaaaaaaaaaaaaac',
+        dareCount: 0,
+        truthCount: 1,
+        tasks: [
+            {
+                _id: 'aaaaaaaaaaaaaaaaaaaaaaba',
+                status: Status.ACTIVE,
+                type: TaskType.TRUTH,
+                currentPlayerGender: CurrentPlayerGender.ANYONE,
+                message: '1234567890'
+            }
+        ]
+    },
+    {
+        _id: 'aaaaaaaaaaaaaaaaaaaaaaaf',
+        status: Status.DELETED,
+        language: Language.EN,
+        category: SetCategory.CLASSIC,
+        visibility: Visibility.PRIVATE,
+        played: 0,
+        name: 'User Set3',
+        createdBy: 'aaaaaaaaaaaaaaaaaaaaaaac',
+        dareCount: 0,
+        truthCount: 1,
+        tasks: [
+            {
+                _id: 'aaaaaaaaaaaaaaaaaaaaaaba',
+                status: Status.ACTIVE,
+                type: TaskType.TRUTH,
+                currentPlayerGender: CurrentPlayerGender.ANYONE,
+                message: '1234567890'
+            }
+        ]
+    }
+];
 export const getMockSet = () => ({
     language: Language.DE,
     name: 'Set number 0',
