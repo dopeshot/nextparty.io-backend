dd2d1648db9636275b6f5c5a7bc562338952309b
Lars Flieger <a.purenetwork@gmail.com>
2021-12-29 20:21:16 +0100

    Update ports from 3000 to 3001

--- src/main.ts
+++ src/main.ts
@@ -6,30 +6,35 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.setGlobalPrefix('api')
-  
-  app.setBaseViewsDir(join(__dirname, '..', 'views'))
-  app.setViewEngine('ejs')
-  app.useGlobalPipes(new ValidationPipe({whitelist: true}))
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    app.setGlobalPrefix('api');
 
-  const config = new DocumentBuilder()
-    .setTitle('Truth or Dare: Community Backend')
-    .setDescription('Back! End! Back! End! Back! End! End!')
-    .setVersion('0.1')
-    .addTag('auth', 'All related to authorization')
-    .addTag('category', 'Categories are only for admins. Categories contains Sets')
-    .addTag('report', 'Report User, Task or Set')
-    .addTag('search')
-    .addTag('set', 'Set is a collection of task')
-    .addTag('system')
-    .addTag('task', 'Task is either a Truth or Dare')
-    .addTag('user', 'User related content')
-    .build()
+    app.setBaseViewsDir(join(__dirname, '..', 'views'));
+    app.setViewEngine('ejs');
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('swagger', app, document)
-  app.enableCors()
-  await app.listen(+process.env.PORT || 3000, () => Logger.log(`Nest listing on ${process.env.HOST}`, 'Bootstrap'))
+    const config = new DocumentBuilder()
+        .setTitle('Truth or Dare: Community Backend')
+        .setDescription('Back! End! Back! End! Back! End! End!')
+        .setVersion('0.1')
+        .addTag('auth', 'All related to authorization')
+        .addTag(
+            'category',
+            'Categories are only for admins. Categories contains Sets'
+        )
+        .addTag('report', 'Report User, Task or Set')
+        .addTag('search')
+        .addTag('set', 'Set is a collection of task')
+        .addTag('system')
+        .addTag('task', 'Task is either a Truth or Dare')
+        .addTag('user', 'User related content')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
+    app.enableCors();
+    await app.listen(+process.env.PORT || 3001, () =>
+        Logger.log(`Nest listing on ${process.env.HOST}`, 'Bootstrap')
+    );
 }
 bootstrap();
