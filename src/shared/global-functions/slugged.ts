5a66ff3a0dcbd186266b025ad1d2d0200c083dd7
Max <48368581+coffemakingtoaster@users.noreply.github.com>
2022-01-15 13:04:58 +0100

    remove Sünde

--- src/shared/global-functions/slugged.ts
+++ src/shared/global-functions/slugged.ts
@@ -6,7 +6,6 @@ export function slugged(string: string): string {
     // Maximum length should be not too large and is thus stripped
     if (string.length > maxSlugLength) {
         const stringArr = string.slice(0, maxSlugLength).split(' ');
-        console.log(stringArr.slice(0, -1).join(' '));
         return stringArr.length > 1
             ? slugged(stringArr.slice(0, -1).join(' ').toString())
             : slugged(stringArr[0]);
