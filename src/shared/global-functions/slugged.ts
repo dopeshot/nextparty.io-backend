10146008a55f8f190a071d550776d071e28e6669
Lars Flieger <a.purenetwork@gmail.com>
2022-01-15 14:36:36 +0100

    Ref slugged

--- src/shared/global-functions/slugged.ts
+++ src/shared/global-functions/slugged.ts
@@ -3,16 +3,18 @@ import slugify from 'slugify';
 export const maxSlugLength = 50;
 
 export function slugged(string: string): string {
-    // Maximum length should be not too large and is thus stripped
-    if (string.length > maxSlugLength) {
-        const stringArr = string.slice(0, maxSlugLength).split(' ');
-        return stringArr.length > 1
-            ? slugged(stringArr.slice(0, -1).join(' ').toString())
-            : slugged(stringArr[0]);
-    }
-
-    return slugify(string, {
+    const sluggedString = slugify(string, {
         remove: /[*+~.()\\/'"!:@]/g,
         lower: true
     });
+
+    // Maximum length should be not too large and is thus stripped
+    if (sluggedString.length > maxSlugLength) {
+        const stringArr = sluggedString.slice(0, maxSlugLength).split('-');
+        return stringArr.length > 1
+            ? stringArr.slice(0, -1).join('-')
+            : stringArr[0];
+    }
+
+    return sluggedString;
 }
